8f7afecfd743bebd9f7cdcd13a0affd02b13e32b
Alexander Berezovsky <a.b@usa.com>
2013-02-13 13:36:56 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayCloud66::Application.config.secret_token = 'f3d2ffa30e822e10c69a1ee0fc1869a4c538fad99c64d7eff24b0590362876f7bcfd8cabb95274b2ff411ee9f88d1591883f9562c69c29e32e1effd8ae48bb66'
